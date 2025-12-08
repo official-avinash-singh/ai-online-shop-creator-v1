5f290d45dc2a3d0b9156f13c2702aa9a24b6fd28
official-avinash-singh <business.singhavinash@gmail.com>
2025-12-08 16:39:47 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import './assets/styles/layout.styles.scss';
+function App() {
+
+  return (
+    <>
+      <h1 className="text-center section-width">This is App component.</h1>
+    </>
+  )
+}
+
+export default App
